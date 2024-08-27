d64c97344c08ba967536856cab1de0bb9c10206c
RomaHome <highvoltage779@gmail.com>
2024-08-27 22:29:32 +0300

    add test case

--- client/src/shared/sum/sum.spec.ts
+++ client/src/shared/sum/sum.spec.ts
@@ -3,6 +3,7 @@ import { sum } from './sum';
 describe('sum function', () => {
     it('should return the correct sum of two numbers', () => {
         expect(sum(1, 2)).toBe(3);
+        expect(sum(2, 2)).toBe(3);
         expect(sum(-1, -1)).toBe(-2);
         expect(sum(0, 0)).toBe(0);
         expect(sum(1.5, 2.5)).toBe(4);
